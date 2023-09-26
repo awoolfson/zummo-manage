c6b64b0fe788b875b6e53fb76f0e2b0d14073df8
Auden Woolfson <auden.woolfson@gmail.com>
2023-09-26 15:39:46 -0400

    get by id working

--- src/get_services.py
+++ src/get_services.py
@@ -1,30 +1,24 @@
 from pyairtable import Api
-import re
 from config import AIRTABLE_PAT, AIRTABLE_TEST_BASE_ID, AIRTABLE_TEST_SERVICES
 
-def get_services_by_phone(phone_number: str) -> list:
+def get_services_by_id(id: str) -> list:
     api = Api(AIRTABLE_PAT)
     services_tbl = api.table(AIRTABLE_TEST_BASE_ID, AIRTABLE_TEST_SERVICES)
     services = services_tbl.all()
     
-    print(services)
-    
-    phone_number = re.sub(r'[^0-9]', '', phone_number)
-    
     matches = []
     
     for service in services:
-        if other := service.get('fields', {}).get('Phone Number'):
-            other = re.sub(r'[^0-9]', '', other)
-            if other == phone_number:
+        if other := service.get('fields', {}).get('Service Bike ID'):
+            if other == id:
                 matches.append(service)
     
     return matches
 
 def main():
     if __name__ == "__main__":
-        # get user input for phone number
-        phone_number = input("Enter a phone number: ")
-        print(get_services_by_phone(phone_number))
+        # get user input for ID
+        id = int(input("Enter a service id: "))
+        print(get_services_by_id(id))
         
 main()
