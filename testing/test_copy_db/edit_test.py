0ce7d41bad73563ec46e267c4b0700964d146cd4
John-Asaro <johnnyasaro63@gmail.com>
2023-10-23 20:34:19 -0400

    added an option to only delete 1 entry

--- testing/test_copy_db/edit_test.py
+++ testing/test_copy_db/edit_test.py
@@ -5,23 +5,32 @@ from pyairtable import Api
 import re
 import requests as rq
 from pprint import pprint
+import json
 #not using flask just yet
 
-#inital vars
+#inital vars - pyairtable
 api = Api(AIRTABLE_PAT) #personal access token
 test_tbl = api.table(AIRTABLE_TEST_BASE_ID, AIRTABLE_TEST_TABLE) #info about test table
 full_tbl = test_tbl.all()
+
+#initial vars - airtable api
+url = f'https://api.airtable.com/v0/{AIRTABLE_TEST_BASE_ID}/{AIRTABLE_TEST_TABLE}'
 
 def add_to_db(usr_field: str, usr_update: str):
     usr_edit = {usr_field: usr_update}
     test_tbl.create(usr_edit)
 
-def delete_from_db(item_id: str):
-    usr_delete = {"id": item_id}
+def clear_id(item_id: str): #deletes every entry of a certain id
     test_tbl.delete(item_id)
 
+def delete_item(item_id: str, usr_field: dict): #replaces a single entry
+    updated_records = {"records": [{"id": item_id,
+     "fields": {usr_field: None}}]}
+    headers = {'Authorization': f'Bearer {AIRTABLE_PAT}','Content-Type': 'application/json'}
+    data = json.dumps(updated_records)
+    response = rq.request("PATCH", url, headers=headers, data=data)
+
 def get_fields(): 
-    url = f'https://api.airtable.com/v0/{AIRTABLE_TEST_BASE_ID}/{AIRTABLE_TEST_TABLE}'
     headers = {'Authorization': f'Bearer {AIRTABLE_PAT}'}
     response = rq.get(url, headers=headers)
     data = response.json()
@@ -35,14 +44,24 @@ def main():
             # asks user for field name and what they would to add to it
             fields = get_fields()
             print(f"These are the field names {fields}")
-            fieldname = input("Enter field name as they appear in the string here: ")
+            fieldname = input("Enter field name as they appear in the list here: ")
             newitem = input("Enter what you would like to add to the field here: ")
             add_to_db(fieldname, newitem)
         elif answer == "d":
-            print(f"This is a refrence you can use to find the id of the item you would like to delete")
-            pprint(full_tbl)
-            item_id = input("What is the id of the item you would like to delete: ")
-            delete_from_db(item_id)
+            answer2 = input("Would you like to delete every item of a single id (1), or a single entry of a single id (2)?")
+            if answer2 == "1":
+                print(f"This is a refrence you can use to find the id and field of the item you would like to delete")
+                pprint(full_tbl)
+                item_id = input("What is the id you would like to clear?: ")
+                clear_id(item_id)
+            elif answer2 == "2":    
+                print(f"This is a refrence you can use to find the id and field of the item you would like to delete")
+                pprint(full_tbl)
+                item_id = input("What is the id of the item you would like to delete: ")
+                fieldname = input("What is the field of the item you would like to delete: ") #going to replace the item corresponding with this id/field combination w/null
+                delete_item(item_id, fieldname)
+            else:
+                main()
         else:
             main()
 main()
