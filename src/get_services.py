07ae7c2302a9d125185fd622959753d65313b609
John-Asaro <johnnyasaro63@gmail.com>
2023-11-06 19:56:25 -0500

    updated get_services

--- src/get_services.py
+++ src/get_services.py
@@ -1,5 +1,7 @@
 from pyairtable import Api
 from config import AIRTABLE_PAT, AIRTABLE_TEST_BASE_ID, AIRTABLE_TEST_SERVICES
+import requests as rq
+from pprint import pprint
 
 def get_services_by_id(id: str) -> list:
     api = Api(AIRTABLE_PAT)
@@ -7,13 +9,39 @@ def get_services_by_id(id: str) -> list:
     services = services_tbl.all()
     
     matches = []
-    
+    response = "" #string put in matches
+    #id <- input (service id of bike)
+    #response = name/phone number
+
     for service in services:
         if other := service.get('fields', {}).get('Service Bike ID'):
             if other == id:
-                matches.append(service)
-    
+                response += (f"The name of the person working on this is {service.get('fields', {}).get('First Name (from Phone Number)')}, ")
+                recordid = service.get('fields', {}).get('Phone Number')[0] #get the id for the record that has the phone number, it will be a one member list so index 0 converts it to string
+                response += (f"the phone number of the person working on this is {extract_number_from_recordid(recordid)}.")
+                matches.append(response)
     return matches
+
+def extract_number_from_recordid(recordid):
+    url = f'https://api.airtable.com/v0/{AIRTABLE_TEST_BASE_ID}/{AIRTABLE_TEST_SERVICES}/{recordid}'
+    headers = {'Authorization': f'Bearer {AIRTABLE_PAT}'}
+    response = rq.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        number = data['fields']['Phone Number'] #the targetted record -> phone number -> return corresponding key
+        return number
+    else:
+        return "not found"
+
+def extract_info_from_record(recordid): #use this if you want everything stored in a particular record, ex: all the info stored in the phone number section
+    url = f'https://api.airtable.com/v0/{AIRTABLE_TEST_BASE_ID}/{AIRTABLE_TEST_SERVICES}/{recordid}' 
+    headers = {'Authorization': f'Bearer {AIRTABLE_PAT}'}
+    response = rq.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
 
 def main():
     if __name__ == "__main__":
