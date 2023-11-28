2a52e2e711a33e82fa50b3db77e4753e5b08d696
JohnAsaro <115180666+JohnAsaro@users.noreply.github.com>
2023-11-28 16:27:47 -0500

    added generate_id_from_service_id

--- src/generate_new_id.py
+++ src/generate_new_id.py
@@ -17,6 +17,19 @@ def generate_new_service_id():
     print(f"Error: {response.status_code}") #you got this error
     return 0 #return id of 0 if we didin't get any id's 
 
+def get_id_from_service_id(service_id: int):
+    url = f"https://api.airtable.com/v0/{AIRTABLE_TEST_BASE_ID}/{AIRTABLE_TEST_SERVICES}"
+    headers = {'Authorization': f'Bearer {AIRTABLE_PAT}'}
+    response = rq.get(url, headers=headers)
+    if response.status_code == 200: #if we get a response
+        data = response.json().get('records', []) #give us a json of whats inside records
+        for record in data:
+            fields = record.get('fields', {})
+            if fields.get('Service Bike ID') == service_id:
+                return record['id']  # Return the id of the matching record
+    else:
+        return f"error {response.status_code}" #return error code if there is an error
+
 def main():
         if __name__ == "__main__":
             print(f"Our new id is {generate_new_service_id()}") #prints out our new id
